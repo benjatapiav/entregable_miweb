d4e05048be4a7afbc6757eaeca56de22fd68f358
benjatapiav <benja.tapiav@duocuc.cl>
2024-05-13 00:32:43 -0400

    cambios agregados

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,9 @@
 const contenedorVinilosChilenos = document.getElementById("productos-container")
+const unidadesElement = document.getElementById("unidades");
+const precioElement = document.getElementById("precio");
+const carritoVacioElement = document.getElementById("carrito-vacio");
+const totalesElement = document.getElementById("totales");
+
 
 function crearTarjetasProductosChilenos(){
     contenedorVinilosChilenos.innerHTML = "";
@@ -21,13 +26,52 @@ function crearTarjetasProductosChilenos(){
         `;
         contenedorVinilosChilenos.appendChild(nuevoViniloChilenos);
         nuevoViniloChilenos
-            .getElementsByTagName("button")[1]
-            .addEventListener("click",(e)=>{
-                const cuentaElement = e.target.parentElement.getElementsByClassName("cantidad")[0];
-                cantidadElement.innerText = restarAlCarrito(producto);
-            crearTarjetasProductosCarrito();
-            actualizarTotales();
+            .getElementsByTagName("button")[1].addEventListener("click",(e)=> {
+                
+                const cuentaElement = e.target.parentElement.getElementsByTagName("span")[0];
+                cuentaElement.innerText = agregarAlCarrito(producto) ;
+                actualizarTotales();
     });
-}
-} 
+        nuevoViniloChilenos
+            .getElementsByTagName("button")[0].addEventListener("click",(e)=> { 
+                restarAlCarrito(producto) ;
+                crearTarjetasProductosChilenos();
+                actualizarTotales();
+                
+                
+        });
+            
+
+    });
+}}
+
+ 
 crearTarjetasProductosChilenos();
+actualizarTotales();
+
+
+function actualizarTotales(){
+    const productos = JSON.parse(localStorage.getItem("vinilosChilenos"));
+    let unidades = 0;
+    let precio = 0;
+    if(productos && productos.length >= 0){
+        productos.forEach(producto => {
+            unidades += producto.cantidad;
+            precio += producto.precio * producto.cantidad;
+        })
+        unidadesElement.innerText = unidades;
+        precioElement.innerText = precio;
+    }
+}
+
+crearTarjetasProductosChilenos();
+
+function revisarMensajeVacio(){
+    const productos = JSON.parse(localStorage.getItem("vinilosChilenos"));
+    carritoVacioElement.classList.toggle("escondido",productos && productos.length > 0);
+    totalesElement.classList.toggle("escondido",!(productos && productos.length > 0));
+}
+
+revisarMensajeVacio();
+
+
--- js/funcion_carrito.js
+++ js/funcion_carrito.js
@@ -1,25 +1,41 @@
 function agregarAlCarrito(producto){
     const memoria = JSON.parse(localStorage.getItem("vinilosChilenos"));
     console.log(memoria);
+    let cuenta = 0;
     if(!memoria){
         const nuevoProducto = producto;
         nuevoProducto.cantidad = 1;
         localStorage.setItem("vinilosChilenos",JSON.stringify([nuevoProducto]));
-    }else{
-        const indiceProducto = memoria.findIndex(vinilochileno => vinilochileno.id);
+        cuenta = 1;
+    } else {
+        const indiceProducto = memoria.findIndex(vinilochileno => vinilochileno.id == producto.id);
         console.log(indiceProducto)
         const nuevaMemoria = memoria;
         if(indiceProducto === -1){
-        nuevaMemoria.push(getNuevoProductoParaMemoria(producto))
-
-        }   else {
+            nuevaMemoria.push(getNuevoProductoParaMemoria(producto))
+            cuenta = 1 ;
+        } else {
             nuevaMemoria[indiceProducto].cantidad ++;
+            cuenta = nuevaMemoria[indiceProducto].cantidad;
         }
         localStorage.setItem("vinilosChilenos",JSON.stringify(nuevaMemoria));
     }
-actualizarNumeroCarrito();    
+    actualizarNumeroCarrito();    
+    return cuenta;
 }
 
+function restarAlCarrito(producto){
+    const memoria = JSON.parse(localStorage.getItem("vinilosChilenos"));
+    const indiceProducto = memoria.findIndex(vinilochileno => vinilochileno.id == producto.id);
+    if(memoria[indiceProducto].cantidad === 1){
+        memoria.splice(indiceProducto,1);
+        localStorage.setItem("vinilosChilenos",JSON.stringify(memoria));
+    }else{
+        memoria[indiceProducto].cantidad -- ;
+    }
+    localStorage.setItem("vinilosChilenos",JSON.stringify(memoria));
+    actualizarNumeroCarrito();
+}
 
 function getNuevoProductoParaMemoria(producto){
     const nuevoProducto = producto;
@@ -29,8 +45,12 @@ function getNuevoProductoParaMemoria(producto){
 const contadorCarritoElement= document.getElementById("contador-carrito");
 function actualizarNumeroCarrito(){
     const memoria = JSON.parse(localStorage.getItem("vinilosChilenos"));
-    const cuenta = memoria.reduce((acum, current) => acum+current.cantidad,0);
-    contadorCarritoElement.innerText = cuenta;
+    if(memoria && memoria.length >0){
+        const cuenta = memoria.reduce((acum, current) => acum+current.cantidad,0);
+        contadorCarritoElement.innerText = cuenta;
+    }else{
+        contadorCarritoElement.innerText = 0;
+    }
 }
 
 actualizarNumeroCarrito();
